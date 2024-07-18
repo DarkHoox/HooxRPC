dd0c65f382f65827f9650a95aada678b1e7d89c1
DarkHoox <127826148+DarkHoox@users.noreply.github.com>
2024-07-18 21:37:51 +0200

    Add files via upload

--- server/main.lua
+++ server/main.lua
@@ -1,16 +1,16 @@
 SetConvarReplicated("sv_maxclients", "true")
 
-PerformHttpRequest("https://raw.githubusercontent.com//respectdevelopment/versions/master/DiscordRPC", function(err, text, headers)
+PerformHttpRequest("https://raw.githubusercontent.com/DarkHoox/versions/main/DiscordRPC", function(err, text, headers)
 
     if text then
 
         local ScriptVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
 
         if ScriptVersion == text then
-            print("^2[INFO] rd_DiscordRPC have latest version! ("..ScriptVersion..")^7")
+            print("^2[INFO] hoox_DiscordRPC have latest version! ("..ScriptVersion..")^7")
         else
-            print("^3[UPDATE] Update for rd_DiscordRPC is avaible! ("..ScriptVersion.. " -> " ..text.. ")^7")
-            print("^3[UPDATE] https://github.com/respectdevelopment/rd_DiscordRPC^7")
+            print("^3[UPDATE] Update for hoox_DiscordRPC is avaible! ("..ScriptVersion.. " -> " ..text.. ")^7")
+            print("^3[UPDATE] https://github.com/DarkHoox/HooxRPC^7")
         end
 
     else
